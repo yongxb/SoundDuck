ddc230c11de7996df5e7283262776692deb17002
xianbin_yong <xianbin.yong13@sps.nus.edu.sg>
2023-06-21 16:19:41 +0800

    Add Data Preprocessing script

--- src/data_module/data_processor.py
+++ src/data_module/data_processor.py
@@ -1,10 +1,240 @@
 """Data processing modules"""
-
-from pathlib import Path
 from typing import List
 
-from pydub import AudioSegment
+import librosa
+import os
+import glob
+from typing import Tuple
+import numpy as np
+import pandas as pd
+import skimage
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 
-class AudioSplitter:
-    """Split the audio into chunks"""
+class BirdPresenceDetector:
+    @staticmethod
+    def create_mel_filterbank(
+        sample_rate, frame_len, num_bands, min_freq, max_freq, corner=700
+    ):
+        """
+        Creates a mel filterbank of `num_bands` triangular filters, with the first
+        filter starting at `min_freq` and the last one stopping at `max_freq`.
+        Returns the filterbank as a matrix suitable for a dot product against
+        magnitude spectra created from samples at a sample rate of `sample_rate`
+        with a window length of `frame_len` samples.
+        """
+        # prepare output matrix
+        input_bins = (frame_len // 2) + 1
+        filterbank = np.zeros((input_bins, num_bands))
+
+        # mel-spaced peak frequencies
+        coefficient = 1000 / np.log1p(1000 / corner)
+        min_mel = coefficient * np.log1p(min_freq / corner)
+        max_mel = coefficient * np.log1p(max_freq / corner)
+        spacing = (max_mel - min_mel) / (num_bands + 1)
+        peaks_mel = min_mel + np.arange(num_bands + 2) * spacing
+        peaks_hz = corner * (np.exp(peaks_mel / coefficient) - 1)
+        fft_freqs = np.linspace(0, sample_rate / 2.0, input_bins)
+        peaks_bin = np.searchsorted(fft_freqs, peaks_hz)
+
+        # fill output matrix with triangular filters
+        for b, filt in enumerate(filterbank.T):
+            # The triangle starts at the previous filter's peak (peaks_freq[b]),
+            # has its maximum at peaks_freq[b+1] and ends at peaks_freq[b+2].
+            left_hz, _, right_hz = peaks_hz[b : b + 3]  # b, b+1, b+2
+            left_bin, top_bin, right_bin = peaks_bin[b : b + 3]
+            # Create triangular filter compatible to yaafe
+            filt[left_bin:top_bin] = (fft_freqs[left_bin:top_bin] - left_hz) / (
+                top_bin - left_bin
+            )
+            filt[top_bin:right_bin] = (right_hz - fft_freqs[top_bin:right_bin]) / (
+                right_bin - top_bin
+            )
+            filt[left_bin:right_bin] /= filt[left_bin:right_bin].sum()
+
+        return filterbank
+
+    @staticmethod
+    def calculate_indicator(melspec, factor=3):
+        # calculate signal regions
+        row_median = np.median(melspec, axis=1).reshape(-1, 1)
+        col_median = np.median(melspec, axis=0).reshape(1, -1)
+
+        row_mask = melspec > (row_median * factor)
+        col_mask = melspec > (col_median * factor)
+        mask = row_mask * col_mask
+
+        # perform erosion then dilation
+        mask = skimage.morphology.binary_erosion(mask, footprint=np.ones((4, 4)))
+        mask = skimage.morphology.binary_dilation(mask, footprint=np.ones((4, 4)))
+
+        indicator = np.any(mask, axis=0)
+        indicator = skimage.morphology.binary_dilation(
+            indicator, footprint=np.ones((4,))
+        )
+        indicator = skimage.morphology.binary_dilation(
+            indicator, footprint=np.ones((4,))
+        )
+
+        return indicator
+
+    @staticmethod
+    def concatenate_parts(indicator, y):
+        change = np.where(indicator[:-1] != indicator[1:])[0]
+
+        if indicator[0] == True:
+            left_edge = np.concatenate(([0], change[1::2]))
+            right_edge = change[0::2]
+        else:
+            left_edge = change[::2]
+            right_edge = change[1::2]
+
+        if indicator[-1] == True:
+            right_edge = np.append(right_edge, len(indicator))
+
+        left_timings = left_edge * 512 * 0.75 - 256
+        left_timings = left_timings.astype(int)
+        right_timings = right_edge * 512 * 0.75 - 256
+        right_timings = right_timings.astype(int)
+        parts = [y[j : k + 1] for (j, k) in zip(left_timings, right_timings)]
+        if len(parts) > 0:
+            output_y = np.concatenate(parts)
+            return output_y
+
+    @classmethod
+    def separate_signal_from_noise(cls, y, sr):
+        x_hann = librosa.stft(
+            y, window="hann", n_fft=512, win_length=512, hop_length=int(512 * 0.75)
+        )
+        melfb = cls.create_mel_filterbank(
+            sample_rate=sr,
+            frame_len=512,
+            num_bands=64,
+            min_freq=150,
+            max_freq=15000,
+            corner=1750,
+        ).T
+        melspec: np.ndarray = np.einsum(
+            "...ft,mf->...mt", np.abs(x_hann), melfb, optimize=True
+        )
+
+        signal_indicator = cls.calculate_indicator(melspec, factor=3)
+
+        noise_indicator = cls.calculate_indicator(melspec, factor=2.5)
+        noise_indicator = ~noise_indicator
+
+        signal_y = cls.concatenate_parts(signal_indicator, y)
+        noise_y = cls.concatenate_parts(noise_indicator, y)
+
+        return signal_y, noise_y
+
+
+class DataPipeline:
+    def __init__(self, audio_files) -> None:
+        self.bird_presence_detector = BirdPresenceDetector()
+        self.audio_files = audio_files
+
+    @staticmethod
+    def load_audio(filepath: str, sample_rate=48000) -> Tuple[np.ndarray, int]:
+        return librosa.load(filepath, sr=sample_rate, mono=True)
+
+    @staticmethod
+    def write_output(folder, filename, output):
+        filepaths = []
+        for i in range(len(output)):
+            filepath = os.path.join(folder, f"{filename}_{i}.tif")
+            skimage.io.imsave(filepath, output[i])
+            filepaths.append(filepath)
+
+        return filepaths
+
+    def split_audio(self, y, length=384, sr=48000):
+        if len(y) < 512:
+            return []
+        x_hann = librosa.stft(
+            y, window="hann", n_fft=512, win_length=512, hop_length=int(512 * 0.75)
+        )
+        melfb = self.bird_presence_detector.create_mel_filterbank(
+            sample_rate=sr,
+            frame_len=512,
+            num_bands=64,
+            min_freq=150,
+            max_freq=15000,
+            corner=1750,
+        ).T
+        melspec: np.ndarray = np.einsum(
+            "...ft,mf->...mt", np.abs(x_hann), melfb, optimize=True
+        )
+
+        output = []
+        n_samples = melspec.shape[1] // length
+        for i in range(n_samples):
+            output.append(melspec[:, i * length : (i + 1) * length])
+
+        return output
+
+    def run(self):
+        output_df = pd.DataFrame(columns=["filepath", "animal", "is_signal"])
+        omit_count = 0
+        for file in tqdm(self.audio_files):
+            folder, filename = os.path.split(file)
+            filename = os.path.splitext(filename)[0]
+            animal = os.path.split(folder)[-1]
+            split_folder = os.path.realpath(
+                os.path.join(folder, "..", "..", "audio_split")
+            )
+            os.makedirs(split_folder, exist_ok=True)
+            signal_folder = os.path.join(split_folder, "signal")
+            os.makedirs(signal_folder, exist_ok=True)
+            noise_folder = os.path.join(split_folder, "noise")
+            os.makedirs(noise_folder, exist_ok=True)
+
+            y, sr = self.load_audio(file)
+            signal_y, noise_y = self.bird_presence_detector.separate_signal_from_noise(
+                y, sr
+            )
+
+            if signal_y is not None:
+                signal_output = self.split_audio(signal_y, sr=sr)
+                filepaths = self.write_output(signal_folder, filename, signal_output)
+                output_df = pd.concat(
+                    (
+                        output_df,
+                        pd.DataFrame.from_dict(
+                            {
+                                "filepath": filepaths,
+                                "animal": [animal] * len(filepaths),
+                                "is_signal": [True] * len(filepaths),
+                            }
+                        ),
+                    )
+                )
+
+            if noise_y is not None:
+                noise_output = self.split_audio(noise_y, sr=sr)
+                filepaths = self.write_output(noise_folder, filename, noise_output)
+                output_df = pd.concat(
+                    (
+                        output_df,
+                        pd.DataFrame.from_dict(
+                            {
+                                "filepath": filepaths,
+                                "animal": [animal] * len(filepaths),
+                                "is_signal": [False] * len(filepaths),
+                            }
+                        ),
+                    )
+                )
+
+        print(omit_count)
+        output_df = output_df.reset_index(drop=True)
+        output_df.to_csv(os.path.join(split_folder, "data.csv"), index=False)
+
+
+if __name__ == "__main__":
+    audio_folder = "./data/dataset/audio"
+    audio_files = glob.glob(os.path.join(audio_folder, "**", "*.mp3"))
+
+    pipeline = DataPipeline(audio_files)
+    pipeline.run()
